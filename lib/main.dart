860fefc31909dd028678544e9c2f65921c288063
Abhijeet Nayak <abnayak@outlook.com>
2017-07-22 21:17:15 -0700

    project files

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import "package:flutter/material.dart";
+
+void main(List<String> args) {
+  runApp(new MaterialApp(
+    title: "Friendly Chat App",
+    home: new ChatWindow(),
+  ));
+}
+
+class ChatWindow extends StatefulWidget {
+  @override
+  State createState() => new _ChatWindowState();
+}
+
+class _ChatWindowState extends State<ChatWindow> {
+  TextEditingController _textEditingController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(title: new Text("Chat App")),
+        body: _buildTextComposer()
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return new Container(
+        margin: new EdgeInsets.all(8.0),
+        child: _buildTextField()
+    );
+  }
+
+  Widget _buildTextField() {
+    return new TextField(
+      decoration: new InputDecoration(hintText: "Input Text Here"),
+      controller: _textEditingController,
+      onSubmitted: _onSubmitted,
+    );
+  }
+
+  void _onSubmitted(String value) {
+  }
+}
+
+/*class MessageInput extends StatelessWidget {
+  TextEditingController _textEditingController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          new TextField(
+            controller: _textEditingController,
+            onSubmitted: _handleOnSubmit,
+            decoration: new InputDecoration(hintText: "Write your message"),
+          ),
+          new IconButton(icon: new Icon(Icons.send), onPressed: () => _handleOnSubmit(_textEditingController.text))
+        ],
+      ),
+    );
+  }
+
+  void _handleOnSubmit(String value) {
+    _textEditingController.clear();
+  }
+}*/
